bd697898e73f9bb6d1421b897b751f6a86826947
user-faiz <email2faizm@gmail.com>
2023-07-04 17:12:05 +0530

    update index.html and style.css

---
+++ mobile-menu.js
@@ -0,0 +1,18 @@
+const icon = document.querySelector('.nav-bar-icon');
+const close = document.querySelector('.close-link');
+const menuLink = document.querySelector('.mob-menu-links');
+const nav = document.querySelectorAll('.bottom-nav-link');
+
+icon.addEventListener('click', () => {
+  menuLink.style.display = 'flex';
+  icon.style.display = 'none';
+  nav.forEach((n) => n.addEventListener('click', () => {
+    menuLink.style.display = 'none';
+    icon.style.display = 'block';
+  }));
+});
+
+close.addEventListener('click', () => {
+  menuLink.style.display = 'none';
+  icon.style.display = 'block';
+});
