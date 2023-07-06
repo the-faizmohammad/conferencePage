42c5556df90202b137aa916978f47bdf1e6610f1
user-faiz <email2faizm@gmail.com>
2023-07-06 14:26:15 +0530

    update index.html

--- speaker.js
+++ speaker.js
@@ -1,41 +1,41 @@
 const speakers = [
-    {
-        image: 'assets/orly obel.jpg',
-        speakerName: 'Orly Obel',
-        job: 'Doctor of Intelligent Systems Engineering',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam iste aliquid facilis rerum aut minima nam laborum maxime',
-    },
-    {
-        image: 'assets/Babak_Hodjat.png',
-        speakerName: 'Babak Hodjat',
-        job: 'Software Engineer and Specialist',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam iste aliquid facilis rerum aut minima nam laborum maxime',
-    },
-    {
-        image: 'assets/dr ebtesam.jpeg',
-        speakerName: 'Dr. Ebtesam',
-        job: 'Software Developer and Specialist',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam iste aliquid facilis rerum aut minima nam laborum maxime',
-    },
-    {
-        image: 'assets/Lila Ibrahim.jpg',
-        speakerName: 'Lila Ibrahim',
-        job: 'Mobile Application Developer and Specialist',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam iste aliquid facilis rerum aut minima nam laborum maxime',
-    },
-    {
-        image: 'assets/ali agha.jpeg',
-        speakerName: 'Ali Agha',
-        job: 'Expert of Cisco',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam iste aliquid facilis rerum aut minima nam laborum maxime',
-    },
-    {
-        image: 'assets/elon musk.jpeg',
-        speakerName: 'Elon Musk',
-        job: 'Full-Stack Software Developer',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam iste aliquid facilis rerum aut minima nam laborum maxime',
-    },
-];
+        {
+            image: 'assets/orly obel.jpg',
+            speakerName: 'Orly Obel',
+            job: 'AI Research Scientist',
+            description: 'Orly Obel is an AI Research Scientist with expertise in developing intelligent systems for various applications. With a strong background in machine learning and data analysis, she works towards advancing AI technology to solve complex real-world problems.'
+        },
+        {
+            image: 'assets/Babak_Hodjat.png',
+            speakerName: 'Babak Hodjat',
+            job: 'Co-founder and Chief Scientist',
+            description: 'Babak Hodjat is a renowned computer scientist and co-founder of an AI company. With extensive experience in natural language processing and cognitive computing, he specializes in developing cutting-edge AI software and solutions to revolutionize industries.'
+        },
+        {
+            image: 'assets/dr ebtesam.jpeg',
+            speakerName: 'Dr. Ebtesam',
+            job: 'AI Ethics Researcher',
+            description: 'Dr. Ebtesam is a leading expert in AI ethics and responsible AI development. As an advocate for ethical AI practices, she conducts research and advises organizations on implementing AI technologies in a fair, transparent, and unbiased manner.'
+        },
+        {
+            image: 'assets/Lila Ibrahim.jpg',
+            speakerName: 'Lila Ibrahim',
+            job: 'Senior AI Engineer',
+            description: 'Lila Ibrahim is a highly skilled AI engineer specializing in mobile application development. With a focus on leveraging AI algorithms and deep learning techniques, she designs and builds intelligent mobile apps that enhance user experiences and provide personalized services.'
+        },
+        {
+            image: 'assets/ali agha.jpeg',
+            speakerName: 'Ali Agha',
+            job: 'Network Security Specialist',
+            description: 'Ali Agha is an expert in network security, particularly in the field of Cisco systems. With a deep understanding of cybersecurity and threat detection, he works to ensure secure and reliable network infrastructure for organizations.'
+        },
+        {
+            image: 'assets/elon musk.jpeg',
+            speakerName: 'Elon Musk',
+            job: 'Founder and CEO of Tesla and SpaceX',
+            description: 'Elon Musk is a visionary entrepreneur and innovator in the technology industry. As the CEO of Tesla and SpaceX, he utilizes software development and AI technologies to drive advancements in electric vehicles, space exploration, and sustainable energy solutions.'
+        }
+    ];
 
 // Add speakers
 // Add speakers
